ae839225cbdfd247bc43acb9d62fb8a097b6558c
Pontus <37812077+pontuscode@users.noreply.github.com>
2022-01-24 14:34:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://odyssey-tutorial-deployed.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
